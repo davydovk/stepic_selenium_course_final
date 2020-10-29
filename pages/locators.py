2afdb6d5cdd4433c32681d0cb8f15e21722b0f2a
Давыдов Константин Юрьевич <davydov@rosenergo.com>
2020-10-29 17:10:22 +0300

    Set files for lesson 4.3 step 10

--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,25 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    # BASKET_LINK = (By.CSS_SELECTOR, 'a.btn.btn-default')
+    BASKET_LINK = (By.CSS_SELECTOR, "span[class='btn-group'] a")
+
+
+class BasketPageLocators():
+    ITEMS_TO_BUY_NOW = (By.CSS_SELECTOR, ".basket-items")
+    MESSAGE_EMPTY_BASKET = (By.CSS_SELECTOR, '#content_inner')
 
 
 class LoginPageLocators():
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class ProductPageLocators():
@@ -16,8 +28,3 @@ class ProductPageLocators():
     PRODUCT_NAME = (By.CSS_SELECTOR, 'div.col-sm-6.product_main > h1')
     ADD_TO_BASKET_MESSAGE = (By.CSS_SELECTOR, 'div.alertinner > strong')
     BASKET_PRICE = (By.CSS_SELECTOR, 'div.alert div p strong')
-
-
-class BasePageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,16 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_message_empty_basket()
 
 
 def test_guest_can_go_to_login_page(browser):
